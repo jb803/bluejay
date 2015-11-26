25d265c90f933b847b7fd9ad97f67ad0d588f9b8
Jack Brewster <jb803@cam.ac.uk>
2015-11-26 20:42:11 +0000

    Initial commit of the pump branch

---
+++ pump/README.md
@@ -0,0 +1,4 @@
+Pumps
+=======
+
+This folder provides a series of scripts in order to non dimensionally design radial pumps taking into account slip factors. These scripts are then used to produce the design for the fuel and oxidiser pumps
---
+++ pump/pumpCalculator.m
@@ -0,0 +1,6 @@
+%Performs pump design calculations
+
+%Assumes no diffusers in the pump
+
+%Define some dimensionless constants
+sigmaB      = 0.4;   %Blade cavitiation coefficint 0.2<=sigmaB<=0.4 (the larger the more likely cavitation will occur
